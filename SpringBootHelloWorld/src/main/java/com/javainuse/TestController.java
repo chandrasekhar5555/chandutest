9a1098ea3cf6f4f2494ee99b5475f43d3c2e8cc0
chandra sekhar <chandrasekhar.parlapalli@gmail.com>
2019-06-19 16:21:53 +0530

    Project added

---
+++ SpringBootHelloWorld/src/main/java/com/javainuse/TestController.java
@@ -0,0 +1,24 @@
+package com.javainuse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/welcome.html")
+	@ResponseBody
+	public ModelAndView firstPage() {
+		return new ModelAndView("welcome");
+	}
+	@RequestMapping(value="/value",method=RequestMethod.GET)
+	public String getValue()
+	{
+		
+		return "welcome";
+	}
+
+}
